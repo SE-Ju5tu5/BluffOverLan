b39e8a74a8047bfbeabe9c4839a560171fef1cf9
Your Name <you@example.com>
2025-08-18 14:58:14 +0200

    Initial commit

---
+++ logic.js
@@ -0,0 +1,394 @@
+﻿const Color = {
+    HEARTS: 'hearts',
+    DIAMONDS: 'diamonds', 
+    CLUBS: 'clubs',
+    SPADES: 'spades'
+};
+
+const Value = {
+    TWO: { name: '2', value: 2 },
+    THREE: { name: '3', value: 3 },
+    FOUR: { name: '4', value: 4 },
+    FIVE: { name: '5', value: 5 },
+    SIX: { name: '6', value: 6 },
+    SEVEN: { name: '7', value: 7 },
+    EIGHT: { name: '8', value: 8 },
+    NINE: { name: '9', value: 9 },
+    TEN: { name: '10', value: 10 },
+    JACK: { name: 'J', value: 11 },
+    QUEEN: { name: 'Q', value: 12 },
+    KING: { name: 'K', value: 13 },
+    ACE: { name: 'A', value: 14 }
+};
+
+class Card {
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+        this.id = value.name + '_' + color;
+    }
+    
+    toString() {
+        return this.value.name + ' of ' + this.color;
+    }
+    
+    toJSON() {
+        return {
+            id: this.id,
+            value: this.value.name,
+            suit: this.color,
+            numericValue: this.value.value
+        };
+    }
+}
+
+class Deck {
+    constructor() {
+        this.cards = [];
+        this.createDeck();
+    }
+
+    createDeck() {
+        this.cards = [];
+        for (let colorKey in Color) {
+            for (let valueKey in Value) {
+                this.cards.push(new Card(Value[valueKey], Color[colorKey]));
+            }
+        }
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            let temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
+        }
+    }
+
+    draw() {
+        if (this.cards.length === 0) {
+            throw new Error("Deck ist leer!");
+        }
+        return this.cards.pop();
+    }
+
+    size() {
+        return this.cards.length;
+    }
+
+    isEmpty() {
+        return this.cards.length === 0;
+    }
+}
+
+class BluffPlayer {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.hand = [];
+        this.index = null;
+    }
+
+    receiveCard(card) {
+        this.hand.push(card);
+    }
+
+    clearCards() {
+        this.hand = [];
+    }
+
+    getHand() {
+        return this.hand;
+    }
+
+    removeCards(cardIds) {
+        const removedCards = [];
+        cardIds.forEach(cardId => {
+            const index = this.hand.findIndex(card => card.id === cardId);
+            if (index !== -1) {
+                removedCards.push(this.hand.splice(index, 1)[0]);
+            }
+        });
+        return removedCards;
+    }
+
+    hasCards(cardIds) {
+        return cardIds.every(cardId => 
+            this.hand.some(card => card.id === cardId)
+        );
+    }
+
+    getCardCount() {
+        return this.hand.length;
+    }
+}
+
+class BluffGame {
+    constructor(gameId) {
+        this.gameId = gameId;
+        this.deck = new Deck();
+        this.players = [];
+        this.centerPile = [];
+        this.currentPlayerIndex = 0;
+        this.gameState = 'waiting';
+        this.winner = null;
+        this.lastAction = null;
+        this.lastClaim = null;
+        this.canCallBluff = false;
+    }
+
+    addPlayer(id, name) {
+        const player = new BluffPlayer(id, name);
+        player.index = this.players.length;
+        this.players.push(player);
+        return player;
+    }
+
+    removePlayer(playerId) {
+        const index = this.players.findIndex(p => p.id === playerId);
+        if (index !== -1) {
+            this.players.splice(index, 1);
+            this.players.forEach((player, i) => {
+                player.index = i;
+            });
+            return true;
+        }
+        return false;
+    }
+
+    getPlayer(playerId) {
+        return this.players.find(p => p.id === playerId);
+    }
+
+    getCurrentPlayer() {
+        return this.players[this.currentPlayerIndex];
+    }
+
+    getNextPlayer() {
+        const nextIndex = (this.currentPlayerIndex + 1) % this.players.length;
+        return this.players[nextIndex];
+    }
+
+    startGame() {
+        if (this.players.length < 2) {
+            throw new Error("Mindestens 2 Spieler erforderlich");
+        }
+
+        this.gameState = 'playing';
+        this.deck.shuffle();
+        this.dealCards();
+        this.currentPlayerIndex = 0;
+        
+        this.lastAction = {
+            type: 'gameStarted',
+            message: this.getCurrentPlayer().name + ' beginnt!'
+        };
+    }
+
+    dealCards() {
+        let playerIndex = 0;
+        while (!this.deck.isEmpty()) {
+            const card = this.deck.draw();
+            this.players[playerIndex].receiveCard(card);
+            playerIndex = (playerIndex + 1) % this.players.length;
+        }
+    }
+
+    playCards(playerId, cardIds, claimedCount, claimedValue) {
+        if (this.gameState !== 'playing') {
+            throw new Error("Spiel läuft nicht");
+        }
+
+        const player = this.getPlayer(playerId);
+        if (!player) {
+            throw new Error("Spieler nicht gefunden");
+        }
+
+        if (this.getCurrentPlayer().id !== playerId) {
+            throw new Error("Du bist nicht am Zug");
+        }
+
+        if (!player.hasCards(cardIds)) {
+            throw new Error("Du hast diese Karten nicht");
+        }
+
+        if (cardIds.length !== claimedCount) {
+            throw new Error("Anzahl der Karten stimmt nicht überein");
+        }
+
+        // Karten entfernen und in die Mitte legen
+        const playedCards = player.removeCards(cardIds);
+        this.centerPile.push(...playedCards);
+
+        // Behauptung speichern
+        this.lastClaim = {
+            value: claimedValue,
+            count: claimedCount,
+            player: player,
+            actualCards: playedCards
+        };
+
+        this.canCallBluff = true;
+
+        this.lastAction = {
+            type: 'cardsPlayed',
+            player: player.name,
+            claimedCount: claimedCount,
+            claimedValue: claimedValue.name,
+            message: player.name + ' behauptet ' + claimedCount + ' x ' + claimedValue.name + ' gespielt zu haben.'
+        };
+
+        // Prüfe auf Gewinner
+        if (player.getCardCount() === 0) {
+            this.gameState = 'finished';
+            this.winner = player;
+            this.lastAction = {
+                type: 'gameWon',
+                winner: player.name,
+                message: player.name + ' hat gewonnen!'
+            };
+            return;
+        }
+
+        // Nächster Spieler
+        this.nextPlayer();
+    }
+
+    callBluff(callerId) {
+        if (this.gameState !== 'playing') {
+            throw new Error("Spiel läuft nicht");
+        }
+
+        if (!this.canCallBluff || !this.lastClaim) {
+            throw new Error("Kein Bluff zum Anzweifeln da");
+        }
+
+        const caller = this.getPlayer(callerId);
+        if (!caller) {
+            throw new Error("Spieler nicht gefunden");
+        }
+
+        // Nur der nächste Spieler kann Bluff rufen
+        if (this.getCurrentPlayer().id !== callerId) {
+            throw new Error("Du kannst jetzt keinen Bluff rufen");
+        }
+
+        // Überprüfe die tatsächlich gespielten Karten
+        const actualCount = this.lastClaim.actualCards.filter(card => 
+            card.value.value === this.lastClaim.value.value
+        ).length;
+
+        const claimedCount = this.lastClaim.count;
+        const liar = this.lastClaim.player;
+
+        let bluffResult;
+
+        if (actualCount >= claimedCount) {
+            // Wahrheit gesagt - Anzweifler bekommt alle Karten
+            caller.hand.push(...this.centerPile);
+            bluffResult = {
+                type: 'bluffFailed',
+                caller: caller.name,
+                liar: liar.name,
+                actualCount: actualCount,
+                claimedCount: claimedCount,
+                actualCards: this.lastClaim.actualCards.map(card => ({
+                    value: card.value.name,
+                    suit: this.getSuitSymbol(card.color)
+                })),
+                message: liar.name + ' hat die Wahrheit gesagt! ' + caller.name + ' muss ' + this.centerPile.length + ' Karten aufnehmen.'
+            };
+        } else {
+            // Gelogen - Original-Spieler bekommt alle Karten
+            liar.hand.push(...this.centerPile);
+            bluffResult = {
+                type: 'bluffSucceeded',
+                caller: caller.name,
+                liar: liar.name,
+                actualCount: actualCount,
+                claimedCount: claimedCount,
+                actualCards: this.lastClaim.actualCards.map(card => ({
+                    value: card.value.name,
+                    suit: this.getSuitSymbol(card.color)
+                })),
+                message: liar.name + ' hat gelogen! Nur ' + actualCount + ' von ' + claimedCount + ' Karten waren echt. ' + liar.name + ' muss ' + this.centerPile.length + ' Karten aufnehmen.'
+            };
+        }
+
+        // Stapel leeren und neue Runde
+        this.centerPile = [];
+        this.lastClaim = null;
+        this.canCallBluff = false;
+
+        this.lastAction = bluffResult;
+
+        return bluffResult;
+    }
+
+    getSuitSymbol(suit) {
+        const symbols = {
+            hearts: '♥',
+            diamonds: '♦',
+            clubs: '♣',
+            spades: '♠'
+        };
+        return symbols[suit] || suit;
+    }
+
+    nextPlayer() {
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+    }
+
+    getPublicGameState() {
+        return {
+            gameId: this.gameId,
+            gameState: this.gameState,
+            players: this.players.map(p => ({
+                id: p.id,
+                name: p.name,
+                cardCount: p.getCardCount(),
+                index: p.index
+            })),
+            currentPlayerIndex: this.currentPlayerIndex,
+            currentPlayer: this.getCurrentPlayer() ? this.getCurrentPlayer().name : null,
+            centerPileCount: this.centerPile.length,
+            lastAction: this.lastAction,
+            lastClaim: this.lastClaim ? {
+                value: this.lastClaim.value.name,
+                count: this.lastClaim.count,
+                player: this.lastClaim.player.name
+            } : null,
+            winner: this.winner ? this.winner.name : null,
+            canCallBluff: this.canCallBluff
+        };
+    }
+
+    getPlayerGameState(playerId) {
+        const player = this.getPlayer(playerId);
+        const publicState = this.getPublicGameState();
+        
+        if (player) {
+            publicState.playerHand = player.getHand().map(card => card.toJSON());
+            publicState.isCurrentPlayer = this.getCurrentPlayer().id === playerId;
+            
+            // Nur der nächste Spieler kann Bluff rufen
+            if (this.canCallBluff && this.lastClaim) {
+                publicState.canCallBluff = this.getCurrentPlayer().id === playerId;
+            } else {
+                publicState.canCallBluff = false;
+            }
+        }
+        
+        return publicState;
+    }
+}
+
+module.exports = {
+    BluffGame,
+    BluffPlayer,
+    Card,
+    Deck,
+    Color,
+    Value
+};
---
+++ server.js
@@ -0,0 +1,321 @@
+﻿const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+const { BluffGame, Value } = require('./logic.js');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const players = new Map();
+const games = new Map();
+
+function getLocalIpAddress() {
+    const { networkInterfaces } = require('os');
+    const nets = networkInterfaces();
+    
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
+function sendGamesList(targetSocket = null) {
+    const gamesList = Array.from(games.values()).map(game => ({
+        id: game.gameId,
+        host: game.players[0] ? players.get(game.players[0].id)?.name : 'Unbekannt',
+        playerCount: game.players.length,
+        maxPlayers: 6,
+        gameState: game.gameState
+    }));
+    
+    if (targetSocket) {
+        targetSocket.emit('gamesList', gamesList);
+    } else {
+        io.emit('gamesList', gamesList);
+    }
+}
+
+io.on('connection', (socket) => {
+    console.log('✅ Spieler verbunden:', socket.id);
+    
+    players.set(socket.id, {
+        id: socket.id,
+        name: 'Spieler_' + socket.id.substring(0, 4),
+        gameId: null,
+        ready: false
+    });
+    
+    socket.emit('playerConnected', {
+        playerId: socket.id,
+        player: players.get(socket.id)
+    });
+    
+    sendGamesList(socket);
+    
+    socket.on('refreshGames', () => {
+        sendGamesList(socket);
+    });
+    
+    socket.on('changeName', (data) => {
+        const player = players.get(socket.id);
+        if (player && data.name && data.name.trim().length > 0) {
+            player.name = data.name.trim().substring(0, 20);
+            socket.emit('nameChanged', { name: player.name });
+            
+            if (player.gameId) {
+                const game = games.get(player.gameId);
+                if (game) {
+                    io.to(player.gameId).emit('gameUpdate', game.getPublicGameState());
+                }
+            }
+        }
+    });
+    
+    socket.on('createGame', (data) => {
+        console.log('🎮 Erstelle Spiel für:', socket.id);
+        const gameId = 'bluff_' + Date.now();
+        const game = new BluffGame(gameId);
+        
+        const player = players.get(socket.id);
+        game.addPlayer(socket.id, player.name);
+        player.gameId = gameId;
+        
+        games.set(gameId, game);
+        socket.join(gameId);
+        
+        socket.emit('gameCreated', { 
+            gameId, 
+            gameState: game.getPublicGameState() 
+        });
+        
+        sendGamesList();
+        console.log('🃏 Spiel erstellt:', gameId);
+    });
+    
+    socket.on('joinGame', (data) => {
+        console.log('👥 Spieler tritt bei:', socket.id, 'Spiel:', data.gameId);
+        const game = games.get(data.gameId);
+        if (!game) {
+            socket.emit('error', { message: 'Spiel nicht gefunden' });
+            return;
+        }
+        
+        if (game.players.length >= 6) {
+            socket.emit('error', { message: 'Spiel ist voll' });
+            return;
+        }
+        
+        if (game.gameState === 'playing') {
+            socket.emit('error', { message: 'Spiel läuft bereits' });
+            return;
+        }
+        
+        const player = players.get(socket.id);
+        
+        if (game.getPlayer(socket.id)) {
+            socket.emit('error', { message: 'Du bist bereits in diesem Spiel' });
+            return;
+        }
+        
+        game.addPlayer(socket.id, player.name);
+        player.gameId = data.gameId;
+        
+        socket.join(data.gameId);
+        io.to(data.gameId).emit('gameUpdate', game.getPublicGameState());
+        sendGamesList();
+    });
+    
+    socket.on('playerReady', () => {
+        console.log('🔄 Spieler bereit:', socket.id);
+        const player = players.get(socket.id);
+        if (!player || !player.gameId) {
+            return;
+        }
+        
+        const game = games.get(player.gameId);
+        if (!game || game.gameState !== 'waiting') {
+            return;
+        }
+        
+        player.ready = !player.ready;
+        
+        const gamePlayers = game.players.map(p => players.get(p.id));
+        const allReady = gamePlayers.every(p => p && p.ready);
+        
+        const playersWithReady = gamePlayers.map(p => ({
+            id: p.id,
+            name: p.name,
+            ready: p.ready
+        }));
+        
+        io.to(player.gameId).emit('lobbyUpdate', {
+            players: playersWithReady,
+            allReady: allReady
+        });
+        
+        if (allReady && game.players.length >= 2) {
+            try {
+                console.log('🎲 Starte Spiel:', game.gameId);
+                game.startGame();
+                gamePlayers.forEach(p => p.ready = false);
+                
+                io.to(player.gameId).emit('gameStarted', game.getPublicGameState());
+                
+                game.players.forEach(p => {
+                    const playerSocket = io.sockets.sockets.get(p.id);
+                    if (playerSocket) {
+                        playerSocket.emit('playerGameState', game.getPlayerGameState(p.id));
+                    }
+                });
+                
+            } catch (error) {
+                console.error('❌ Fehler beim Spielstart:', error);
+                io.to(player.gameId).emit('error', { message: error.message });
+            }
+        } else {
+            io.to(player.gameId).emit('gameUpdate', game.getPublicGameState());
+        }
+    });
+    
+    socket.on('playCards', (data) => {
+        console.log('🃏 Karten spielen Event:', socket.id);
+        
+        const player = players.get(socket.id);
+        if (!player || !player.gameId) {
+            socket.emit('error', { message: 'Du bist in keinem Spiel' });
+            return;
+        }
+        
+        const game = games.get(player.gameId);
+        if (!game) {
+            socket.emit('error', { message: 'Spiel nicht gefunden' });
+            return;
+        }
+        
+        try {
+            game.playCards(socket.id, data.cardIds, data.claimedCount, data.claimedValue);
+            
+            console.log('✅ Karten erfolgreich gespielt!');
+            
+            // Alle über die neue Situation informieren
+            io.to(player.gameId).emit('gameUpdate', game.getPublicGameState());
+            
+            // Jedem Spieler seinen aktuellen Zustand senden
+            game.players.forEach(p => {
+                const playerSocket = io.sockets.sockets.get(p.id);
+                if (playerSocket) {
+                    playerSocket.emit('playerGameState', game.getPlayerGameState(p.id));
+                }
+            });
+            
+        } catch (error) {
+            console.error('❌ Fehler beim Karten spielen:', error.message);
+            socket.emit('error', { message: error.message });
+        }
+    });
+    
+    socket.on('callBluff', () => {
+        console.log('🚨 Bluff gerufen von:', socket.id);
+        const player = players.get(socket.id);
+        if (!player || !player.gameId) return;
+        
+        const game = games.get(player.gameId);
+        if (!game) return;
+        
+        try {
+            const bluffResult = game.callBluff(socket.id);
+            console.log('✅ Bluff abgearbeitet');
+            
+            // Bluff-Ergebnis an alle senden
+            io.to(player.gameId).emit('bluffResult', bluffResult);
+            
+            // Game State Update
+            io.to(player.gameId).emit('gameUpdate', game.getPublicGameState());
+            
+            // Jedem Spieler seinen neuen Zustand senden
+            game.players.forEach(p => {
+                const playerSocket = io.sockets.sockets.get(p.id);
+                if (playerSocket) {
+                    playerSocket.emit('playerGameState', game.getPlayerGameState(p.id));
+                }
+            });
+            
+        } catch (error) {
+            console.error('❌ Fehler beim Bluff rufen:', error.message);
+            socket.emit('error', { message: error.message });
+        }
+    });
+    
+    socket.on('leaveGame', () => {
+        const player = players.get(socket.id);
+        if (player && player.gameId) {
+            const game = games.get(player.gameId);
+            if (game) {
+                game.removePlayer(socket.id);
+                socket.leave(player.gameId);
+                player.gameId = null;
+                player.ready = false;
+                
+                if (game.players.length === 0) {
+                    games.delete(player.gameId);
+                    console.log('🗑️ Leeres Spiel gelöscht');
+                } else {
+                    io.to(game.gameId).emit('gameUpdate', game.getPublicGameState());
+                }
+                
+                sendGamesList();
+            }
+        }
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('❌ Spieler getrennt:', socket.id);
+        
+        const player = players.get(socket.id);
+        if (player && player.gameId) {
+            const game = games.get(player.gameId);
+            if (game) {
+                game.removePlayer(socket.id);
+                
+                if (game.players.length === 0) {
+                    games.delete(player.gameId);
+                } else {
+                    io.to(player.gameId).emit('gameUpdate', game.getPublicGameState());
+                }
+                
+                sendGamesList();
+            }
+        }
+        
+        players.delete(socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+const LOCAL_IP = getLocalIpAddress();
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log('=====================================');
+    console.log('🃏 BLUFF KARTENSPIEL SERVER LÄUFT!');
+    console.log('=====================================');
+    console.log(`Lokal: http://localhost:${PORT}`);
+    console.log(`LAN: http://${LOCAL_IP}:${PORT}`);
+    console.log('=====================================');
+});
